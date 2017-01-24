fbb4cc145b0f4fa273a2cb779f9f2f4e52d9acd5
John O'Keefe <jokeefe@fastmail.com>
2017-01-24 10:05:30 -0600

    added a fetch function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Leaderboard from './Components/leaderboard';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    function top30(){
+      return fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then(response => response.json())
+      .then(data => );
+    }
+
+    this.state = {
+      leaderboard: [
+        {
+          rank: '1',
+          camperName: 'Manish-Girl',
+          points30: '699',
+          pointsAll: '3596'
+        },
+        {
+          rank: '2',
+          camperName: 'diomed',
+          points30: '484',
+          pointsAll: '2338'
+        },
+        {
+          rank: '3',
+          camperName: 'JohnPaulWalsh',
+          points30: '484',
+          pointsAll: '883'
+        },
+        {
+          rank: '4',
+          camperName: 'sjames1958gm',
+          points30: '480',
+          pointsAll: '5156'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        <Leaderboard />
       </div>
     );
   }
---
+++ src/Components/leaderboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Leaderboard extends Component {
+
+  render() {
+    return (
+      <div className="Leaderboard">
+        My Leaderboard
+      </div>
+    );
+  }
+}
+
+export default Leaderboard;
