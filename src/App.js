c8e8ac9f07d780023138f684e76b6e54164c04bd
John O'Keefe <jokeefe@fastmail.com>
2017-01-26 20:46:18 -0600

    slowly figuring out how to use the data.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
 import Leaderboard from './Components/leaderboard';
+// import axios from 'axios';
 import './App.css';
 
-  const fccCharts = [{    
-    fccRecent: fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(response => response.json()),
-    fccAllTime: fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime').then(response => response.json())
-  }]
+  const fccRecent = fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(response => response.json()).then(json => json);
+  const example = fetch('http://codepen.io/jobs.json').then(response => response.json()).then(json => json)
+  // axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent").then(result => console.log(result))
+  const fccAllTime = fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime').then(response => response.json()).then(json => json);
 
 class App extends Component {
   constructor(props){
     super(props);
-
-
     this.state = {
-      recent: fccCharts[0].fccRecent,
-      allTime: fccCharts[0].fccAllTime
+      recent: [],
+      allTime: []
     };
-
-    // const getRecent = new Promise((resolve, reject) => {
-    //
-    // });
-
-//watch video on promises to combine multiple api calls!!!
-    // Promise.all(fccCharts)
-    // .then((response) => {
-    //   this.setState = {
-    //     recent: response[0].fccRecent,
-    //     allTime: response[0].fccAllTime
-    //   }
-    // })
-
-  }
+}
+componentDidMount() {
+  let _this = this;
+  this.AllRequests = Promise.all([fccRecent, fccAllTime, example]).then(values => {
+    _this.setState({
+      recent: values[0],
+      allTime: values[1]
+    })
+  })
+}
   render() {
+    // <Leaderboard allTime={this.state.allTime} recent={this.state.recent} />
+    // console.log(this.state.recent)
+
+    //getting tip in http://mediatemple.net/blog/tips/loading-and-using-external-data-in-react/
     return (
       <div className="App">
-        My App
-        <Leaderboard allTime={this.state.allTime} recent={this.state.recent} />
+        <h1>People</h1>
+        {this.state.recent.map(person => {
+          return console.log(person)
+        })}
+
       </div>
     );
   }
--- src/Components/leaderboard.js
+++ src/Components/leaderboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Leaderboard = (props) => {
-	console.log(props.recent);
-  	console.log(props.allTime);
+	// console.log(props.recent.then(x => x.map(y => y.username)));
+  // 	console.log(props.allTime);
   // console.log(props.rank.map(x => x.username))
     // {props.rank.map(x => x.username)}
+		// {props.recent.then(x => x.map(y => y.username))}
   return (
-    <div>Name: 
+    <div>Recent Name: {props.recent}
     </div>
   )
 }
