9630b6672152eb85cc8b8bc573a4f03dcf7d2d68
John O'Keefe <jokeefe@fastmail.com>
2017-01-28 14:45:57 -0600

    Getting started with tables. Research

--- src/Components/leaderboard.js
+++ src/Components/leaderboard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Leaderboard = (props) => {
-	let recentSorter = (bool) => {
+	let Sorter = (bool) => {
 		if (bool){
 			return (a,b) => b.recent - a.recent;
 		}else{
 			return (a,b) => a.recent - b.recent;
 		}
 	}
-	let recentButtonSort = recentSorter(true);
+	let recentButtonSort = Sorter(true);
 
-	
+
 	if(!props.recent){
 		return console.log("Loading...")
 	}else{
@@ -19,12 +19,38 @@ const Leaderboard = (props) => {
 	console.log(recentName)
   return (
     <div>
-	    <h1>Recent Name:</h1>
-	    <div>
-	    	<ul>
-	    		{ recentName.sort(recentButtonSort).map(x => <li>Name: <strong>{x.username}</strong> Recent: <strong>{x.recent}</strong></li>) }
-	    	</ul>
-	    </div>
+			<table>
+				<tbody>
+	      <tr>
+	        <th>
+	          Camper Name
+	        </th>
+	        <th>
+	          Points in the past 30 days
+	        </th>
+	        <th>
+	          All time points
+	        </th>
+	      </tr>
+	      <tr>
+					{/*Check this out for help creating this thing:
+					http://stackoverflow.com/questions/39462458/react-js-creating-simple-table
+					Also google search: https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=make+a+table+in+react */
+				}
+	        <td>{ recentName.sort(recentButtonSort).map(x => x.username) }</td>
+					<td>{ recentName.sort(recentButtonSort).map(x => x.recent) }</td>
+		      <td>{ recentName.sort(recentButtonSort).map(x => x.allTime) }</td>
+	      </tr>
+				</tbody>
+	    </table>
+			{
+			// <h1>FreeCodeCamp Leaderboard</h1>
+			// <div>
+			// 	<ul>
+			// 		{ recentName.sort(recentButtonSort).map(x => <li>Name: <strong>{x.username}</strong> Recent: <strong>{x.recent}</strong></li>) }
+			// 	</ul>
+			// </div>
+		}
     </div>
   )
 }
